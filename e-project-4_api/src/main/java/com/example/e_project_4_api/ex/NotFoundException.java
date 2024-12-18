b6489117ad79246273e2625fd16397278daf2f32
kokomashimaro <136688499+kokomashimaro@users.noreply.github.com>
2024-12-18 18:39:26 +0700

    delete files

--- e-project-4_api/src/main/java/com/example/e_project_4_api/ex/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.e_project_4_api.ex;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String msg) {
-        super(msg);
-    }
-}
--- e-project-4_api/src/main/java/com/example/e_project_4_api/utilities/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.e_project_4_api.utilities;
-
-public enum Role {
-    ROLE_ADMIN,
-    ROLE_USER,
-    ROLE_ARTIST;
-}
