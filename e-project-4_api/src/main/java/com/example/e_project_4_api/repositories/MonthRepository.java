9c429eeeb05de17bb4bb9aa782c694bb301cc51f
manh <truongducmanh07092004@gmail.com>
2025-02-13 21:44:02 +0700

    delete api

--- e-project-4_api/src/main/java/com/example/e_project_4_api/repositories/MonthRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.e_project_4_api.repositories;
-
-import com.example.e_project_4_api.models.MonthOfYear;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MonthRepository extends JpaRepository<MonthOfYear, Integer> {
-}
